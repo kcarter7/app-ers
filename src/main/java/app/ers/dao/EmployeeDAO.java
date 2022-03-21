f4e5a8c0ec7303c0a910bdcc7e1804e07a55f75d
Kaylin Carter <kcarte22@mail.umw.edu>
2022-03-21 00:35:33 -0400

    adding functionality to employee

--- src/main/java/app/ers/dao/EmployeeDAO.java
+++ src/main/java/app/ers/dao/EmployeeDAO.java
@@ -3,6 +3,7 @@ package app.ers.dao;
 import java.util.List;
 
 import app.ers.model.Employee;
+import app.ers.model.Reimbursement;
 
 public interface EmployeeDAO {
 
@@ -10,4 +11,8 @@ public interface EmployeeDAO {
 	public boolean validate(String username, String password);
 	public List<Employee> getEmployees();
 	public List<Employee> getEmployeeByUserName(String username);
+	public boolean reimbursementRequest(Reimbursement reimbursement);
+	public List<Reimbursement> viewPendingReimbursement(int employeeId, String status);
+	public List<Reimbursement> viewResolveReimbursement(int employeeId, String status);
+	
 }
